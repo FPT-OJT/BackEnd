7acbeb543b0d7db07f18c2add1115d5a18bc6299
Mirai3103 <huuhoag1412@gmail.com>
2026-02-02 15:44:11 +0700

    implement card search API with full-text search capabilities

--- src/main/java/com/fpt/ojt/repositories/card/CardProductRepository.java
+++ src/main/java/com/fpt/ojt/repositories/card/CardProductRepository.java
@@ -3,10 +3,23 @@ package com.fpt.ojt.repositories.card;
 import com.fpt.ojt.models.postgres.card.CardProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CardProductRepository extends JpaRepository<CardProduct, UUID>, JpaSpecificationExecutor<CardProduct> {
+    @Query(value = """
+                SELECT *
+                FROM card_products
+                WHERE deleted_at IS NULL
+                  AND search_text % lower(unaccent(:q))
+                ORDER BY search_text <-> lower(unaccent(:q)) ASC
+                LIMIT :limit
+            """, nativeQuery = true)
+    List<CardProduct> search(@Param("q") String keyword,
+            @Param("limit") int limit);
 }
--- src/main/java/com/fpt/ojt/services/card/CardService.java
+++ src/main/java/com/fpt/ojt/services/card/CardService.java
@@ -1,10 +1,13 @@
 package com.fpt.ojt.services.card;
 
 import com.fpt.ojt.services.dtos.AvailableCardRulesDto;
+import com.fpt.ojt.services.dtos.CardProductDto;
 
 import java.util.List;
 import java.util.UUID;
 
+
 public interface CardService {
     List<AvailableCardRulesDto> getAvailableCardRulesByUserId(UUID userId);
+    List<CardProductDto> searchCardProducts(String keyword,int limit);
 }
