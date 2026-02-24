7e5b853d148a9a8b070bee0641a52c12db6da719
Mirai3103 <huuhoag1412@gmail.com>
2026-02-24 09:11:01 +0700

    Enhance RedisConfig and JwtTokenProviderImpl by adding conditional bean registration and lazy initialization for RedisTemplate and RedisScript, improving dependency management and configuration flexibility.

--- src/main/java/com/fpt/ojt/infrastructure/configs/RedisConfig.java
+++ src/main/java/com/fpt/ojt/infrastructure/configs/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.fpt.ojt.infrastructure.configs;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 @EnableRedisRepositories(basePackages = "com.fpt.ojt.repositories")
+@ConditionalOnBean(RedisConnectionFactory.class)
 public class RedisConfig {
 
     @Bean
